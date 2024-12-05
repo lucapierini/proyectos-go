b3dfdeac935a79340c15d6a7bb31eed2c210080e
Luk <lucapierini03@gmail.com>
2024-12-05 02:29:18 -0300

    Creación de primeras estructuras

---
+++ e-comerce/api/model/user.go
@@ -0,0 +1,16 @@
+package model
+
+import "time"
+
+type User struct {
+	ID        uint      `gorm:"primaryKey"`
+	FirstName string    `gorm:"size:100;not null"`
+	LastName  string    `gorm:"size:100;not null"`
+	Email     string    `gorm:"uniqueIndex;size:100;not null"`
+	Password  string    `gorm:"not null"`
+	Phone     string    `gorm:"size:15"`
+	Role      string    `gorm:"default:'customer';not null"` // customer o admin
+	Address   []Address `gorm:"foreignKey:UserID"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
